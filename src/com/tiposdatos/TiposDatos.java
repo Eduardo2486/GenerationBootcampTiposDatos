3e37961122927c82d95536860677d597aaebbeb4
Eduardo2486 <eduardo0137@protonmail.com>
2021-07-15 17:12:42 -0500

    primer commit

---
+++ src/com/tiposdatos/TiposDatos.java
@@ -0,0 +1,25 @@
+package com.tiposdatos;
+
+public class TiposDatos {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world!");
+		
+		int numberOne = 10;
+		int NomberTwo = 5;
+		int multiply = numberOne * NomberTwo;
+		String stringOne ="text 1";
+		String stringTwo = "text 2";
+		char singleLetter = 'A';
+		
+		String concat = stringOne + stringTwo;
+		
+		boolean olderThan18 = true;
+		
+		System.out.println(concat);
+		System.out.println(multiply);
+		System.out.println(singleLetter);
+		System.out.println(olderThan18);
+	}
+}
